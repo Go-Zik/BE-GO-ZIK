418e5bca7937534533465022abe769bd133f3077
Wooram Hong <coadhs1@naver.com>
2023-03-04 15:52:11 +0900

    first commit

---
+++ src/main/java/com/example/gozik/GozikApplication.java
@@ -0,0 +1,13 @@
+package com.example.gozik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GozikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GozikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gozik/GozikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gozik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GozikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
