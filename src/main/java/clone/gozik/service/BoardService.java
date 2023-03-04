442d10d5666c7d11220d64a93c42bba491c0adcb
Galmaeki <kjh09800@gmail.com>
2023-03-04 22:15:18 +0900

    feat: 메인화면 get 구현

--- src/main/java/clone/gozik/service/BoardService.java
+++ src/main/java/clone/gozik/service/BoardService.java
@@ -10,6 +10,9 @@ import clone.gozik.repository.BoardRepository;
 import clone.gozik.repository.JobRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -36,6 +39,16 @@ public class BoardService {
     @Value("ap-northeast-2")
     private String region;
 
+    @Transactional(readOnly = true)
+    public List mainBoard() {
+       Pageable mainpage = PageRequest.of(1,27);
+       Page<Board> boardList = boardRepository.findAll(mainpage);
+       List<MainBoardResponseDto> boardResponse = new ArrayList<>();
+        for (Board board : boardList) {
+            boardResponse.add(new MainBoardResponseDto(board));
+        }
+        return boardResponse;
+    }
 
 
     @Transactional(readOnly = true)
@@ -70,11 +83,7 @@ public class BoardService {
         //임시 코드 1 > 구현시 모두 제거
         String nickname = "임시닉네임 로그인구현시 변경";
         Member member = new Member();//임시멤버, 멤버 리포지토리 구현시 제거
-
-//        String imageurl = "임시이미지 url";
-//        String logourl = "로고 url";
         //임시코드1은 여기까지
-
         //파일 저장하며 url 받아오기
         String imageurl = s3Uploader.upload(image, "image");
         String logourl = s3Uploader.upload(logo, "logo");
@@ -98,10 +107,9 @@ public class BoardService {
         //임시 코드 2 > 구현시 모두 제거
         String nickname = "임시닉네임 로그인구현시 변경";
         Member member = new Member();//임시멤버, 멤버 리포지토리 구현시 제거
+        //임시코드2는 여기까지
         String imageurl = "";
         String logourl = "";
-        //임시코드2는 여기까지
-
         if (image.equals("")||image.equals(board.getImage())){
             imageurl = board.getImage();
         }else {
@@ -112,7 +120,6 @@ public class BoardService {
         }else {
             logourl = s3Uploader.upload(logo, "logo");
         }
-
 
         LocalDate startDate = extractDate(boardRequestDto.getStartDate());//String에서 날짜추출
         if(boardRequestDto.isRecruitmentPeriod()){
@@ -138,4 +145,6 @@ public class BoardService {
     private LocalDate extractDate(String date){
         return LocalDate.parse(date, DateTimeFormatter.ISO_LOCAL_DATE);
     }
+
+
 }
