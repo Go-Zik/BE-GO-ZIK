6ecbb5349d3f28ab15f9b79dec4299350078ebab
Galmaeki <kjh09800@gmail.com>
2023-03-07 11:43:34 +0900

    fix: job detail 수정

--- src/main/java/clone/gozik/entity/Job.java
+++ src/main/java/clone/gozik/entity/Job.java
@@ -37,17 +37,7 @@ public class Job {
             case "CONTRACT" -> CONTRACT;
             default -> throw new IllegalArgumentException("채용 형태를 확인해주세요" );
         };
-        this.board = board;
-    }
-
-    public Job(RequestJobDto requestJobDto){
-        this.incruittype = switch (requestJobDto.getIncruittype()){
-            case "신입" -> NEW;
-            case "인턴" -> INTERN;
-            case "경력" -> EXPERIENCED;
-            case "CONTRACT" -> CONTRACT;
-            default -> throw new IllegalArgumentException("채용 형태를 확인해주세요" );
-        };
         this.jobDetail = requestJobDto.getJobdetail();
+        this.board = board;
     }
 }
